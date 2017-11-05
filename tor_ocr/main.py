ddf29274e5552b9b80f90da0c7a7fb39d9bc3b3e
Perry Fraser <perry@frasers.org>
2017-11-05 13:26:51 -0500

    Add find and replace for >> in 4chan greentext and all that

--- tor_ocr/main.py
+++ tor_ocr/main.py
@@ -184,6 +184,8 @@ def run(config):
                     '/u/', '\\/u/'
                 ).replace(
                     '/r/', '\\/r/'
+                ).replace(
+                    '>>', '\>\>'
                 )
             )
         )
