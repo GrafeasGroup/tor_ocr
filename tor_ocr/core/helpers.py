73044242a3d07e53f5f08528ca1eb6f34ed5abf9
Joe Kaufeld <joe.kaufeld@gmail.com>
2019-08-05 22:03:30 -0400

    update clean-id for new api
    Signed-off-by: Joe Kaufeld <joe.kaufeld@gmail.com>

--- tor_ocr/core/helpers.py
+++ tor_ocr/core/helpers.py
@@ -144,10 +144,16 @@ def clean_id(post_id):
     `t3_`, but this doesn't always work for getting a new object. This
     method removes those prefixes and returns the rest.
 
+    Sometimes we might be working with an ID string that's already clean;
+    just return that sucker.
+
     :param post_id: String. Post fullname (ID)
     :return: String. Post fullname minus the first three characters.
     """
-    return post_id[post_id.index('_') + 1:]
+    try:
+        return post_id[post_id.index('_') + 1:]
+    except ValueError:
+        return post_id
 
 
 def get_parent_post_id(post, r):
